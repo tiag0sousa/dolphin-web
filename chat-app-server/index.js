e6f73dde3191d3a365bb9aa8d2525c908f7bf204
tiagosousa <tiago.fariasousa@farfetch.com>
2023-05-15 09:55:11 +0100

    Update port

--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = 3001;
+const port = process.env.PORT ||3001;
 
 app.use(cors());
 app.use(express.json());
